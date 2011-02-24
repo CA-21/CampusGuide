bcaa0c7a5ff1c238549468b1e1bed5c355f278aa
Ira Ray Jenkins <irarayjenkins@gmail.com>
2011-02-24 14:50:45 -0500

    Initial setup.

---
+++ src/cs/mobile/team4/campusguide/CampusGuideActivity.java
@@ -0,0 +1,13 @@
+package cs.mobile.team4.campusguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CampusGuideActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
